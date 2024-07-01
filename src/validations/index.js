f573fb5e8d8d8bbff2021f9f8eca4389503ce755
hello-world-ttj <tijotjoseph003@gmail.com>
2024-07-01 11:55:59 +0530

    #status

--- src/validations/index.js
+++ src/validations/index.js
@@ -24,6 +24,7 @@ exports.createRoleSchema = Joi.object({
   description: Joi.string(),
   permissions: Joi.array(),
   locationAccess: Joi.array(),
+  status: Joi.boolean(),
 });
 
 exports.editRoleSchema = Joi.object({
