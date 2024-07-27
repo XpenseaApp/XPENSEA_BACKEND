69398f32c0d85596316eec88dc9e0f634c0758df
hello-world-ttj <tijotjoseph003@gmail.com>
2024-07-27 15:47:57 +0530

    #status

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1041,7 +1041,7 @@ exports.deleteUser = async (req, res) => {
 
     const deleteUser = await User.findByIdAndUpdate(
       id,
-      { isDeleted: true, deletedAt: new Date() },
+      { isDeleted: true, deletedAt: new Date(), status: false },
       { new: true }
     );
     if (deleteUser) {
