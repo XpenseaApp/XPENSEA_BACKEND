761f19831c8afb7622c857f1cc3bda68c7c4b355
Nihal anil <43931373+CntrlX@users.noreply.github.com>
2024-08-03 10:18:46 +0530

    test 5 chore: Include user type in response message for getExpense function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -588,7 +588,7 @@ exports.getExpense = async (req, res) => {
       date: moment(expense.createdAt).format("MMM DD YYYY"),
     };
 
-    return responseHandler(res, 200, "Expense found", mappedData);
+    return responseHandler(res, 200, user.userType+"Expense found", mappedData);
   } catch (error) {
     return responseHandler(res, 500, `Internal Server Error ${error.message}`);
   }
