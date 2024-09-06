f2fda7065aec6c5ba50f0ec177143bc59b70b160
Nihal anil <43931373+CntrlX@users.noreply.github.com>
2024-09-06 17:32:48 +0530

    change to google cloud

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,49 +1,22 @@
-const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
-/**
- * Loads secrets from Google Cloud Secret Manager and sets them as environment variables.
- * This method will retrieve the latest version of the secret by default.
- */
 const loadSecrets = async () => {
-  // Define the secret version path (ensure you replace 'YOUR_PROJECT_ID' and 'prod-xpensea')
-  const secretName = "projects/393541516579/secrets/prod_xpensea/versions/latest";
-  
-  // Create a Secret Manager client
   const client = new SecretManagerServiceClient();
+  const secretName = 'projects/393541516579/secrets/prod_xpensea/versions/latest';
 
   try {
-    // Construct the request for accessing the secret version
-    const [accessResponse] = await client.accessSecretVersion({
-      name: secretName,
-    });
+    const [accessResponse] = await client.accessSecretVersion({ name: secretName });
+    const payload = accessResponse.payload.data.toString('utf8');
+    const secrets = JSON.parse(payload);
 
-    // Get the payload (secret data) as a string
-    const payload = accessResponse.payload.data.toString("utf8");
-
-    if (payload) {
-      // Parse the payload into a JSON object
-      const secrets = JSON.parse(payload);
-
-      // Set each key-value pair as an environment variable
-      for (const [key, value] of Object.entries(secrets)) {
-        process.env[key] = value;
-      }
-
-      console.log("Secrets successfully loaded and set as environment variables.");
-    } else {
-      console.error("No payload found in the secret.");
+    for (const [key, value] of Object.entries(secrets)) {
+      process.env[key] = value;
     }
+
+    console.log('Secrets successfully loaded and set as environment variables.');
   } catch (error) {
-    // Enhanced error handling with more detailed logging
-    if (error.code === 5) {
-      console.error("Secret not found. Please check your project ID, secret name, and access permissions.");
-    } else if (error.code === 7) {
-      console.error("Permission denied. Ensure that your Google Cloud credentials have access to Secret Manager.");
-    } else {
-      console.error(`Error retrieving secrets: ${error.message}`);
-    }
-
-    throw new Error(`Error retrieving secrets: ${error.message}`);
+    console.error('Error retrieving secrets:', error);
+    throw new Error('Error retrieving secrets: ' + error.message);
   }
 };
 
